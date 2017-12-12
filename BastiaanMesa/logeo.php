0e967b00e84018c7a3a87cb9718c5f87b211598e
ingbr <ingbr@LAPTOP-ED29U7B5>
2017-12-12 11:34:29 -0600

    Formato

--- BastiaanMesa/logeo.php
+++ BastiaanMesa/logeo.php
@@ -17,14 +17,15 @@ $CNUSERDESC = REQUEST('CNUSERDESC');
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <link rel="stylesheet" href="css/materialize.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<title>Basstian</title>
+<title>Mesa de ayuda</title>
 </head>
 <body  style="padding-left:10%; padding-right:10%">
      <div class="row">
          <div class="col s12">
-              <header align="center" class="card-panel orange darken-2 accent-3 z-depth-4">
-                    <h1 class="white-text">Bastiaan Tiket</h1>
+              <header align="center" class="card-panel deep-orange darken-1 z-depth-4">
+                    <h3 class="white-text">Bastiaan Tiket</h3>
               </header>
          </div>
      </div>
@@ -32,14 +33,17 @@ $CNUSERDESC = REQUEST('CNUSERDESC');
 <form class="form-horizontal" method="POST" action="">
      <div class="card-panel white z-depth-4">
           <div class="form-group">
-              <label for="inputEmail3" class="col-sm-2 control-label">ID USUARIO</label>
-                   <div class="col-sm-10">
-                       <input type="" class="form-control" name="usuario" placeholder="ID User" >
-                  </div>
+          
+               <label class="col-sm-2 control-label">User</label>
+               
+          
+              <!--   <i class="material-icons prefix">account_circle</i> -->
+               <input id="icon_prefix" type="text"  placeholder="ID Usuario"  class="validate">
+                
           </div>
 <!-- Otro-->
 <div class="form-group">
-     <label for="inputPassword3" class="col-sm-2 control-label">CONTRASEÑA</label>
+     <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
           <div class="col-sm-10">
               <input type="password" class="form-control" name="contra"  placeholder="Password" >
          </div>
@@ -56,7 +60,7 @@ $CNUSERDESC = REQUEST('CNUSERDESC');
 </div>
       <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Enviar</button>
+                <button  align="center" type="submit" class="btn btn-default">Enviar</button>
            </div>
      </div>
 </form>
